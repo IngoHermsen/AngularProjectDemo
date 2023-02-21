8e1406dda06de1b414dff1cc8dc252305b0ab672
Ingo Hermsen <ingohermsen@gmail.com>
2023-02-21 18:31:25 +0100

    added images and worked on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularWebsite';
+export class AppComponent implements OnInit {
+  images = ['DSC03907.jpg', 'DSC03928.jpg', 'DSC03940.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1)
+    }, 8000)
+  }
 }
